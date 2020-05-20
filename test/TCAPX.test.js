5f367d8d46215961de6bddc1842d5f60599330b3
Cristian Espinoza <ceegarner@gmail.com>
2020-05-20 16:36:44 -0600

    Completed tests

--- test/TCAPX.test.js
+++ test/TCAPX.test.js
@@ -15,6 +15,7 @@ describe("TCAP.x Token", async function () {
 		if (owner && addr1 && handler) {
 			accounts.push(await owner.getAddress());
 			accounts.push(await addr1.getAddress());
+			accounts.push(await handler.getAddress());
 			handlerAddress = await handler.getAddress();
 		}
 	});
@@ -76,9 +77,50 @@ describe("TCAP.x Token", async function () {
 		expect(currentHandler).to.eq(handlerAddress);
 	});
 
-	xit("...should allow Handler to mint tokens", async () => {});
+	it("...should allow Handler to mint tokens", async () => {
+		const amount = ethersProvider.utils.parseEther("100");
+		await expect(tcapInstance.connect(handler).mint(accounts[1], amount))
+			.to.emit(tcapInstance, "Transfer")
+			.withArgs(ethersProvider.constants.AddressZero, accounts[1], amount);
+		const balance = await tcapInstance.balanceOf(accounts[1]);
+		expect(balance).to.eq(amount);
+		const totalSupply = await tcapInstance.totalSupply();
+		expect(totalSupply).to.eq(amount);
+	});
 
-	xit("...should allow Handler to burn tokens", async () => {});
+	it("...should allow users to transfer", async () => {
+		const amount = ethersProvider.utils.parseEther("100");
+		const bigAmount = ethersProvider.utils.parseEther("10000");
+		await expect(tcapInstance.connect(owner).transfer(accounts[1], amount)).to.be.revertedWith(
+			"ERC20: transfer amount exceeds balance"
+		);
+		await expect(tcapInstance.connect(addr1).transfer(accounts[0], bigAmount)).to.be.revertedWith(
+			"ERC20: transfer amount exceeds balance"
+		);
+		await expect(tcapInstance.connect(addr1).transfer(accounts[0], amount))
+			.to.emit(tcapInstance, "Transfer")
+			.withArgs(accounts[1], accounts[0], amount);
+		let balance = await tcapInstance.balanceOf(accounts[1]);
+		expect(balance).to.eq(0);
+		balance = await tcapInstance.balanceOf(accounts[0]);
+		expect(balance).to.eq(amount);
+	});
 
-	xit("...should allow users to transfer", async () => {});
+	it("...should allow Handler to burn tokens", async () => {
+		const amount = ethersProvider.utils.parseEther("100");
+		const bigAmount = ethersProvider.utils.parseEther("10000");
+		await expect(tcapInstance.connect(handler).burn(accounts[0], bigAmount)).to.be.revertedWith(
+			"ERC20: burn amount exceeds balance"
+		);
+		await expect(tcapInstance.connect(handler).burn(accounts[1], amount)).to.be.revertedWith(
+			"ERC20: burn amount exceeds balance"
+		);
+		await expect(tcapInstance.connect(handler).burn(accounts[0], amount))
+			.to.emit(tcapInstance, "Transfer")
+			.withArgs(accounts[0], ethersProvider.constants.AddressZero, amount);
+		const balance = await tcapInstance.balanceOf(accounts[0]);
+		expect(balance).to.eq(0);
+		const totalSupply = await tcapInstance.totalSupply();
+		expect(totalSupply).to.eq(0);
+	});
 });
