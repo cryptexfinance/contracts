7d4a3d52cc7f7b58fc91e40ef2f14783d6fc578d
Cristian Espinoza <ceegarner@gmail.com>
2020-05-20 12:27:02 -0600

    switched tests to js as there is a bug on ts testing suite

--- test/TCAPX.test.js
+++ test/TCAPX.test.js
@@ -1,33 +1,31 @@
-import {expect} from "chai";
-const {ethers} = require("@nomiclabs/buidler");
-import {Tcapx} from "../typechain/Tcapx";
-import "mocha";
-import {TcapxFactory} from "../typechain/TcapxFactory";
-import {ethers as ethersProvider} from "ethers";
-//
+var expect = require("chai").expect;
+var ethersProvider = require("ethers");
+
 describe("TCAP.x Token", async function () {
-	let tcapInstance: Tcapx;
-	let [owner, addr1]: ethersProvider.providers.JsonRpcSigner[] | undefined[] = [
-		undefined,
-		undefined,
-	];
-	let accounts: string[] = [];
-	let handler: string = "0xC257274276a4E539741Ca11b590B9447B26A8051"; //TODO: this should be the handler contract
+	let tcapInstance;
+	let [owner, addr1, handler] = [undefined, undefined];
+	let accounts = [];
+	let handlerAddress = "";
+
 	before("Set Accounts", async () => {
-		let [acc0, acc1] = await ethers.getSigners();
+		let [acc0, acc1, acc3] = await ethers.getSigners();
 		owner = acc0;
 		addr1 = acc1;
-		if (owner && addr1) {
+		handler = acc3;
+		if (owner && addr1 && handler) {
 			accounts.push(await owner.getAddress());
 			accounts.push(await addr1.getAddress());
+			handlerAddress = await handler.getAddress();
 		}
 	});
+
 	it("...should deploy the contract", async () => {
-		const TCAPX: TcapxFactory = await ethers.getContractFactory("TCAPX");
+		const TCAPX = await ethers.getContractFactory("TCAPX");
 		tcapInstance = await TCAPX.deploy("TCAP.X", "TCAPX", 11);
 		await tcapInstance.deployed();
 		expect(tcapInstance.address).properAddress;
 	});
+
 	it("...should set the correct initial values", async () => {
 		const symbol = await tcapInstance.symbol();
 		const name = await tcapInstance.name();
@@ -38,47 +36,49 @@ describe("TCAP.x Token", async function () {
 		expect(name).to.eq("TCAP.X");
 		expect(decimals).to.eq(18, "Decimals should be 18");
 	});
+
 	it("...should have the ERC20 standard functions", async () => {
 		const totalSupply = await tcapInstance.totalSupply();
 		expect(totalSupply).to.eq(0, "Total supply should be 0");
 		const balance = await tcapInstance.balanceOf(accounts[0]);
 		expect(balance).to.eq(0, "Balance should be 0");
 	});
+
 	it("...should allow to approve tokens", async () => {
 		const amount = ethersProvider.utils.parseEther("100");
-		await tcapInstance
-			.connect(owner as ethersProvider.providers.JsonRpcSigner)
-			.approve(accounts[1], amount);
+		await tcapInstance.connect(owner).approve(accounts[1], amount);
 		const allowance = await tcapInstance.allowance(accounts[0], accounts[1]);
 		expect(allowance).to.eq(amount);
 	});
+
 	it("...shouldn't allow users to mint", async () => {
 		const amount = ethersProvider.utils.parseEther("1000000");
 		await expect(tcapInstance.mint(accounts[0], amount)).to.be.revertedWith(
 			"Caller is not the handler"
 		);
 	});
+
 	it("...shouldn't allow users to burn", async () => {
 		const amount = ethersProvider.utils.parseEther("1000000");
 		await expect(tcapInstance.burn(accounts[1], amount)).to.be.revertedWith(
 			"Caller is not the handler"
 		);
 	});
+
 	it("...should allow owner to set Handler", async () => {
-		await expect(
-			tcapInstance
-				.connect(addr1 as ethersProvider.providers.JsonRpcSigner)
-				.setTokenHandler(accounts[1])
-		).to.be.revertedWith("Ownable: caller is not the owner");
-		await expect(
-			tcapInstance.connect(owner as ethersProvider.providers.JsonRpcSigner).setTokenHandler(handler)
-		)
+		await expect(tcapInstance.connect(addr1).setTokenHandler(accounts[1])).to.be.revertedWith(
+			"Ownable: caller is not the owner"
+		);
+		await expect(tcapInstance.connect(owner).setTokenHandler(handlerAddress))
 			.to.emit(tcapInstance, "LogSetTokenHandler")
-			.withArgs(accounts[0], handler);
+			.withArgs(accounts[0], handlerAddress);
 		let currentHandler = await tcapInstance.tokenHandler();
-		expect(currentHandler).to.eq(handler);
+		expect(currentHandler).to.eq(handlerAddress);
 	});
+
 	xit("...should allow Handler to mint tokens", async () => {});
+
 	xit("...should allow Handler to burn tokens", async () => {});
+
 	xit("...should allow users to transfer", async () => {});
 });
