5727082a2d05cab090fc6a67c5e4f8cea6633ba2
Cristian Espinoza <ceegarner@gmail.com>
2020-06-09 16:17:57 -0600

    updated scripts

---
+++ deploy/001_dai_handler.ts
@@ -0,0 +1,26 @@
+module.exports = async ({getNamedAccounts, deployments}: any) => {
+	const {deployIfDifferent, log} = deployments;
+	const {deployer} = await getNamedAccounts();
+
+	let handlerContract;
+	try {
+		handlerContract = await deployments.get("DAITokenHandler");
+	} catch (error) {
+		log(error.message);
+	}
+	if (!handlerContract) {
+		const deployResult = await deployIfDifferent(
+			["data"],
+			"DAITokenHandler",
+			{from: deployer, gas: 4000000},
+			"DAITokenHandler"
+		);
+		handlerContract = await deployments.get("DAITokenHandler");
+		if (deployResult.newlyDeployed) {
+			log(
+				`DAITokenHandler deployed at ${handlerContract.address} for ${deployResult.receipt.gasUsed}`
+			);
+		}
+	}
+};
+module.exports.tags = ["DAITokenHandler"];
