61b40e0f893ef8ae3a1763fc29c83dac253c7c3b
Cristian Espinoza <ceegarner@gmail.com>
2021-06-02 11:15:11 -0600

    add mainnet abi

--- scripts/proposal.ts
+++ scripts/proposal.ts
@@ -1,29 +1,32 @@
 // run with
-// npx hardhat run ./scripts/proposal.ts --network mainnet
+// npx hardhat run ./scripts/proposal.ts --network hardhat
 import hre, { deployments, network, hardhatArguments } from "hardhat";
 
 async function main() {
 	const ethers = hre.ethers;
 	const ONE_DAY = 86500;
+	const amount = ethers.utils.parseEther("25.40428051");
+	const multisign = "0xa70b638B70154EdfCbb8DbbBd04900F328F32c35";
 	console.log("Print proposal DATA for Multisign");
-	let ethVaultHandler = await deployments.get("WETHVaultHandler");
 	let orchestrator = await deployments.get("Orchestrator");
 	const abi = new ethers.utils.AbiCoder();
-	const newRatio = 150;
-	const targets = [orchestrator.address];
+	const targets = [multisign];
 	const values = [0];
-	const signatures = ["setRatio(address,uint256)"];
-	const calldatas = [abi.encode(["address", "uint256"], [ethVaultHandler.address, newRatio])];
-	const description = "Reduce ETH Vault liquidation ratio to 150%";
+	// @ts-ignore
+	const signatures = [""]; // @ts-ignore
+	const calldatas = [0x0];
+	const description = "June Payroll and Grants ";
 	console.log(targets);
 	console.log(values);
 	console.log(signatures);
 	console.log(calldatas);
 	console.log(description);
 
+	let balance = await ethers.provider.getBalance(multisign);
+	console.log("Old Balance is: ", ethers.utils.formatEther(balance));
+
 	if (hardhatArguments.network === "hardhat") {
 		// VB sends us ETH
-
 		await hre.network.provider.request({
 			method: "hardhat_impersonateAccount",
 			params: ["0xab5801a7d398351b8be11c439e05c5b3259aec9b"],
@@ -48,6 +51,7 @@ async function main() {
 		// Create Proposal
 
 		console.log("==================Create Proposal==================");
+		// @ts-ignore
 		let tx = await governorContract.propose(targets, values, signatures, calldatas, description);
 		console.log(tx);
 
@@ -56,7 +60,7 @@ async function main() {
 
 		// Vote
 		console.log("==================Vote==================");
-		tx = await governorContract.castVote(1, true);
+		tx = await governorContract.castVote(2, true);
 		console.log(tx);
 
 		await ethers.provider.send("evm_mine", []);
@@ -64,7 +68,7 @@ async function main() {
 		// Wait to queue
 		console.log("==================Queue==================");
 		//block.number <= proposal.endBlock
-		let proposal = await governorContract.proposals(1);
+		let proposal = await governorContract.proposals(2);
 
 		console.log(".-.-.-.-.-.-Waiting for blocks to mine.-.-.-.-.-.-");
 		for (let i = ethers.provider.blockNumber; i < proposal.endBlock; i++) {
@@ -73,13 +77,13 @@ async function main() {
 		// await ethers.provider.send("evm_increaseTime", [ONE_DAY * 4]);
 		// await ethers.provider.send("evm_mine", []);
 
-		console.log((await governorContract.state(1)).toString());
-		tx = await governorContract.queue(1);
+		console.log((await governorContract.state(2)).toString());
+		tx = await governorContract.queue(2);
 		await ethers.provider.send("evm_mine", []);
 		console.log(tx);
 		// Execute transaction
 		console.log("==================Execute Transaction==================");
-		proposal = await governorContract.proposals(1);
+		proposal = await governorContract.proposals(2);
 
 		// console.log(".-.-.-.-.-.-Waiting for blocks to mine.-.-.-.-.-.-");
 		// for (let i = ethers.provider.blockNumber; i < proposal.eta; i++) {
@@ -87,18 +91,13 @@ async function main() {
 		// }
 		await ethers.provider.send("evm_increaseTime", [ONE_DAY * 4]);
 		await ethers.provider.send("evm_mine", []);
-		tx = await governorContract.execute(1);
+		tx = await governorContract.execute(2);
 		console.log(tx);
 		await ethers.provider.send("evm_mine", []);
 		// Check Ratio
-		console.log("==================Check Ratio==================");
-		let vaultContract = await ethers.getContractAt(
-			"ETHVaultHandler",
-			ethVaultHandler.address,
-			signer
-		);
-		const ratio = await vaultContract.ratio();
-		console.log("RATIO IS: ", ratio.toString());
+		console.log("==================Check Balance==================");
+		balance = await ethers.provider.getBalance(multisign);
+		console.log("New Balance is: ", ethers.utils.formatEther(balance));
 	}
 }
 
