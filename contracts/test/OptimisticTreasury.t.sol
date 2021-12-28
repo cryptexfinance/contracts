d4e4ee5af5dfe166f56b24e7b127c302ae9e1b14
@Crisgarner <yarn>
2021-12-28 08:18:28 -0600

    forge install: ds-test

---
+++ contracts/test/OptimisticTreasury.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.7.5;
+
+import "ds-test/test.sol";
+
+contract OptimisticTreasury is DSTest {
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
