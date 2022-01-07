d90788547bb31b53020df7381994adb874823fa1
@Crisgarner <yarn>
2022-01-07 11:48:38 -0600

    small refactors

--- test.sh
+++ test.sh
@@ -3,5 +3,5 @@ export DAPP_LINK_TEST_LIBRARIES=0
 export DAPP_TEST_SMTTIMEOUT=500000
 #export PROPTEST_CASES=50000
 #export RUST_LOG=evm_adapters=trace
-forge test --verbosity 4 --contracts test --remappings @openzeppelin/=node_modules/@openzeppelin/ @chainlink/=node_modules/@chainlink/ fx-portal-contracts/=node_modules/fx-portal-contracts
+forge test -vvv --contracts test --remappings @openzeppelin/=node_modules/@openzeppelin/ @chainlink/=node_modules/@chainlink/ fx-portal-contracts/=node_modules/fx-portal-contracts
 
