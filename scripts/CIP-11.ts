cdf007997a469b5adc1a6062a5fb0444ab2ed984
@Crisgarner <yarn>
2022-01-10 17:28:15 -0600

    scripts for adding link and aave

--- scripts/CIP-11.ts
+++ scripts/CIP-11.ts
@@ -1,5 +1,5 @@
 // run with
-// npx hardhat run ./scripts/CIP-7-10.ts --network hardhat
+// npx hardhat run ./scripts/CIP-11.ts --network hardhat
 import hre, { deployments, network, hardhatArguments } from "hardhat";
 import { castVote, createProposal, executeProposal, fundMultisign, queueProposal } from "./utils";
 import { BigNumber } from "ethers";
@@ -7,29 +7,32 @@ import { BigNumber } from "ethers";
 async function main() {
 	const ethers = hre.ethers;
 
-	const amount = ethers.utils.parseEther("8100"); // TODO: update this
-	const multisig = "0xa70b638B70154EdfCbb8DbbBd04900F328F32c35";
-	let ctx = await deployments.get("Ctx");
-	let ctxContract = await ethers.getContractAt("Ctx", ctx.address);
 	let orchestratorAddress = "0x373C74BcE7893097ab26d22f05691907D4f2c18e";
 	let tcap = await deployments.get("TCAP");
 	let tcapContract = await ethers.getContractAt("TCAP", tcap.address);
+	let aaveVault = await deployments.get("AaveVaultHandler");
+	let linkVault = await deployments.get("LinkVaultHandler");
 
 	const abi = new ethers.utils.AbiCoder();
-	const targets = [ctx.address];
-	const values = [BigNumber.from(0)];
-	const signatures = ["transfer(address,uint256)"];
-	const calldatas = [abi.encode(["address", "uint256"], [multisig, amount])];
-	const description =
-		"CIP-7: Airdrop early TCAP Testers, CIP-10 Cryptex Community Grants Program Q4";
+	const targets = [orchestratorAddress, orchestratorAddress];
+	const values = [BigNumber.from(0), BigNumber.from(0)];
+	const signatures = ["addTCAPVault(address,address)", "addTCAPVault(address,address)"];
+	const calldatas = [
+		abi.encode(["address", "address"], [tcap.address, aaveVault.address]),
+		abi.encode(["address", "address"], [tcap.address, linkVault.address]),
+	];
+	const description = "CIP-11: Add AAVE and LINK Vaults";
 	console.log(targets);
 	console.log(values);
 	console.log(signatures);
 	console.log(calldatas);
 	console.log(description);
 
-	let balance = await ctxContract.balanceOf(multisig);
-	console.log("multisig old CTX balance", ethers.utils.formatEther(balance));
+	let aaveStatus = await tcapContract.vaultHandlers(aaveVault.address);
+	console.log(aaveStatus);
+
+	let linkStatus = await tcapContract.vaultHandlers(linkVault.address);
+	console.log(linkStatus);
 
 	if (hardhatArguments.network === "hardhat") {
 		//Fund Multisign with ETH
@@ -49,8 +52,11 @@ async function main() {
 
 		// Validate Results
 		console.log("==================Check Results==================");
-		balance = await ctxContract.balanceOf(multisig);
-		console.log("Multisig new CTX balance", ethers.utils.formatEther(balance));
+
+		aaveStatus = await tcapContract.vaultHandlers(aaveVault.address);
+		console.log(aaveStatus);
+		linkStatus = await tcapContract.vaultHandlers(linkVault.address);
+		console.log(linkStatus);
 	}
 }
 
