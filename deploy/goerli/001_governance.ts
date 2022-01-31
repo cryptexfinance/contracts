3beba09b701abebda26de1c76dff9837ad26650d
@Crisgarner <yarn>
2022-01-31 08:44:54 -0600

    fixed goerli scripts

--- deploy/goerli/001_governance.ts
+++ deploy/goerli/001_governance.ts
@@ -3,64 +3,57 @@ import { DeployFunction } from "hardhat-deploy/types";
 import { deployments, hardhatArguments } from "hardhat";
 
 module.exports = async ({ ethers, getNamedAccounts, deployments }: any) => {
+	if (hardhatArguments.network === "goerli") {
+		const Ctx = await deployments.getOrNull("Ctx");
+		const { log } = deployments;
+		const namedAccounts = await getNamedAccounts();
+		const oneYear = 1675175407; // Mon, January 24 2023
+		const threeDays = 259200;
+		const [owner] = await ethers.getSigners();
 
-    if (hardhatArguments.network === "goerli") {
-        const Ctx = await deployments.getOrNull("Ctx");
-        const { log } = deployments;
-				const namedAccounts = await getNamedAccounts();
-				const oneYear = 1643045563; // Mon, January 24 2021
-				const threeDays = 259200;
-				const [owner] = await ethers.getSigners();
+		let nonce = await owner.getTransactionCount();
+		const ctxAddress = ethers.utils.getContractAddress({
+			from: namedAccounts.deployer,
+			nonce: nonce++,
+		});
 
-				let nonce = await owner.getTransactionCount();
-				const ctxAddress = ethers.utils.getContractAddress({
-						from: namedAccounts.deployer,
-						nonce: nonce++,
-				});
+		const timelockAddress = ethers.utils.getContractAddress({
+			from: namedAccounts.deployer,
+			nonce: nonce++,
+		});
 
-				const timelockAddress = ethers.utils.getContractAddress({
-						from: namedAccounts.deployer,
-						nonce: nonce++,
-				});
+		const governorAddress = ethers.utils.getContractAddress({
+			from: namedAccounts.deployer,
+			nonce: nonce++,
+		});
+		try {
+			const ctxDeployment = await deployments.get("Ctx");
+		} catch (error) {
+			log(error.message);
+			const ctxDeployment = await deployments.deploy("Ctx", {
+				from: namedAccounts.deployer,
+				args: [namedAccounts.deployer, timelockAddress, oneYear],
+			});
+			log(`Ctx deployed at ${ctxDeployment.address} for ${ctxDeployment.receipt?.gasUsed}`);
+		}
 
-				const governorAddress = ethers.utils.getContractAddress({
-						from: namedAccounts.deployer,
-						nonce: nonce++,
-				});
-				try {
-					const ctxDeployment = await deployments.get("Ctx");
-				}
-				catch(error) {
-					log(error.message);
-					const ctxDeployment = await deployments.deploy("Ctx", {
-						from: namedAccounts.deployer,
-						args: [namedAccounts.deployer, timelockAddress, oneYear],
-					});
-					log(
-						`Ctx deployed at ${ctxDeployment.address} for ${ctxDeployment.receipt?.gasUsed}`
-					);
-				}
+		const timelockDeployment = await deployments.deploy("Timelock", {
+			from: namedAccounts.deployer,
+			args: [governorAddress, threeDays],
+		});
 
-				const timelockDeployment = await deployments.deploy("Timelock", {
-						from: namedAccounts.deployer,
-						args: [governorAddress, threeDays],
-				});
+		log(
+			`Timelock deployed at ${timelockDeployment.address} for ${timelockDeployment.receipt?.gasUsed}`
+		);
 
-				log(
-						`Timelock deployed at ${timelockDeployment.address} for ${timelockDeployment.receipt?.gasUsed}`
-				);
+		const governorDeployment = await deployments.deploy("GovernorBeta", {
+			from: namedAccounts.deployer,
+			args: [timelockAddress, ctxAddress, namedAccounts.deployer],
+		});
 
-				const governorDeployment = await deployments.deploy(
-						"GovernorAlpha",
-						{
-								from: namedAccounts.deployer,
-								args: [timelockAddress, ctxAddress],
-						}
-				);
-
-				log(
-						`Governor Alpha deployed at ${governorDeployment.address} for ${governorDeployment.receipt?.gasUsed}`
-				);
-    }
+		log(
+			`Governor Beta deployed at ${governorDeployment.address} for ${governorDeployment.receipt?.gasUsed}`
+		);
+	}
 };
 module.exports.tags = ["Governance"];
