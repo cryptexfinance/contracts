9965f1df68a922839ad943d80a3c4b8e5f25665d
Crisgarner <@crisgarner>
2023-01-06 13:43:06 -0600

    merged

--- deploy/arbitrumGoerli/001_message_executor.ts
+++ deploy/arbitrumGoerli/001_message_executor.ts
@@ -15,28 +15,11 @@ module.exports = async ({ ethers, getNamedAccounts, deployments }: any) => {
 	if (hardhatArguments.network !== "arbitrumGoerli") {
 		return
 	}
-<<<<<<< HEAD
-	// // requireEnvVariables(["GOERLI_ARBITRUM_MESSAGE_RELAYER_ADDRESS"]);
-=======
 	requireEnvVariables(["GOERLI_ARBITRUM_MESSAGE_RELAYER_ADDRESS", "GOERLI_TIMELOCK_ADDRESS"]);
->>>>>>> a4d0910dde060b4af77d359de34a7f135583b739
 
-	// const { log } = deployments;
-	// const namedAccounts = await getNamedAccounts();
+	const { log } = deployments;
+	const namedAccounts = await getNamedAccounts();
 
-<<<<<<< HEAD
-	// const l2MessageExecutorDeployment = await deployments.deploy("L2MessageExecutor", {
-	// 	from: namedAccounts.deployer,
-	// 	skipIfAlreadyDeployed: true,
-	// 	log: true,
-	// 	args: [process.env.GOERLI_ARBITRUM_MESSAGE_RELAYER_ADDRESS],
-	// });
-
-	// log(
-	// 	`ArbitrumLL2MessageExecutor deployed at ${l2MessageExecutorDeployment.address}
-	// 	for ${l2MessageExecutorDeployment.receipt?.gasUsed}`
-	// );
-=======
 	const l2MessageExecutorDeployment = await deployments.deploy("L2MessageExecutor", {
 		from: namedAccounts.deployer,
 		skipIfAlreadyDeployed: true,
@@ -66,7 +49,6 @@ module.exports = async ({ ethers, getNamedAccounts, deployments }: any) => {
 			callData
 		]
 	});
->>>>>>> a4d0910dde060b4af77d359de34a7f135583b739
 
 	log(
 		`L2MessageExecutorProxy deployed at ${l2MessageExecutorProxyDeployment.address}
--- deploy/arbitrumGoerli/002_orchestrator.ts
+++ deploy/arbitrumGoerli/002_orchestrator.ts
@@ -5,11 +5,7 @@ module.exports = async ({ getNamedAccounts, deployments }: any) => {
 			return;
 	const { deployIfDifferent, log } = deployments;
 	const { deployer } = await getNamedAccounts();
-<<<<<<< HEAD
-	// const l2MessageExecutorDeployResult = await deployments.get("L2MessageExecutor");
-=======
 	const l2MessageExecutorProxyDeployResult = await deployments.get("L2MessageExecutorProxy");
->>>>>>> a4d0910dde060b4af77d359de34a7f135583b739
 
 	const orchestratorDeployResult = await deployments.deploy("ArbitrumOrchestrator", {
 		from: deployer,
@@ -17,15 +13,10 @@ module.exports = async ({ getNamedAccounts, deployments }: any) => {
 		log: true,
 		// 	Note: Owner is set to deployer so that initial vaults can be
 		// launched without the need for voting
-<<<<<<< HEAD
-		args: [deployer, deployer]
-        // args: [deployer, l2MessageExecutorDeployResult.address, l2MessageExecutorDeployResult.address]
-=======
 		args: [
 			deployer,
 			deployer,
 		]
->>>>>>> a4d0910dde060b4af77d359de34a7f135583b739
 	});
 
 	log(
