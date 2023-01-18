2668a70e57bfd928a3bd9fc4922a923086611eee
Crisgarner <@crisgarner>
2023-01-18 13:27:12 -0600

    running linter

--- test/TCAP.test.js
+++ test/TCAP.test.js
@@ -2,83 +2,83 @@ var expect = require("chai").expect;
 var ethersProvider = require("ethers");
 
 describe("TCAP Token", async function () {
-	let tcapInstance;
-	let orchestratorInstance;
-	let [owner, addr1, handler, handler2, guardian] = [];
-	let accounts = [];
+    let tcapInstance;
+    let orchestratorInstance;
+    let [owner, addr1, handler, handler2, guardian] = [];
+    let accounts = [];
 
-	before("Set Accounts", async () => {
-		let [acc0, acc1, acc3, acc4, acc5, acc6] = await ethers.getSigners();
-		owner = acc0;
-		addr1 = acc1;
-		handler = acc3;
-		handler2 = acc4;
-		guardian = acc6;
-		if (owner && addr1 && handler) {
-			accounts.push(await owner.getAddress());
-			accounts.push(await addr1.getAddress());
-			accounts.push(await handler.getAddress());
-			accounts.push(await handler2.getAddress());
-			accounts.push(await acc5.getAddress());
-		}
-	});
+    before("Set Accounts", async () => {
+        let [acc0, acc1, acc3, acc4, acc5, acc6] = await ethers.getSigners();
+        owner = acc0;
+        addr1 = acc1;
+        handler = acc3;
+        handler2 = acc4;
+        guardian = acc6;
+        if (owner && addr1 && handler) {
+            accounts.push(await owner.getAddress());
+            accounts.push(await addr1.getAddress());
+            accounts.push(await handler.getAddress());
+            accounts.push(await handler2.getAddress());
+            accounts.push(await acc5.getAddress());
+        }
+    });
 
-	it("...should deploy the contract", async () => {
-		const orchestrator = await ethers.getContractFactory("Orchestrator");
-		orchestratorInstance = await orchestrator.deploy(await guardian.getAddress());
-		await orchestratorInstance.deployed();
-		expect(orchestratorInstance.address).properAddress;
+    it("...should deploy the contract", async () => {
+        const orchestrator = await ethers.getContractFactory("Orchestrator");
+        orchestratorInstance = await orchestrator.deploy(await guardian.getAddress());
+        await orchestratorInstance.deployed();
+        expect(orchestratorInstance.address).properAddress;
 
-		let cap = ethers.utils.parseEther("100");
-		const TCAP = await ethers.getContractFactory("TCAP");
-		tcapInstance = await TCAP.deploy(
-			"Total Market Cap Token",
-			"TCAP",
-			cap,
-			orchestratorInstance.address
-		);
-		await tcapInstance.deployed();
-		expect(tcapInstance.address).properAddress;
-	});
+        let cap = ethers.utils.parseEther("100");
+        const TCAP = await ethers.getContractFactory("TCAP");
+        tcapInstance = await TCAP.deploy(
+            "Total Market Cap Token",
+            "TCAP",
+            cap,
+            orchestratorInstance.address
+        );
+        await tcapInstance.deployed();
+        expect(tcapInstance.address).properAddress;
+    });
 
-	it("...should set the correct initial values", async () => {
-		const symbol = await tcapInstance.symbol();
-		const name = await tcapInstance.name();
-		const decimals = await tcapInstance.decimals();
-		const defaultOwner = await tcapInstance.owner();
-		const cap = await tcapInstance.cap();
-		expect(defaultOwner).to.eq(orchestratorInstance.address);
-		expect(symbol).to.eq("TCAP", "Symbol should equal TCAP");
-		expect(name).to.eq("Total Market Cap Token");
-		expect(decimals).to.eq(18, "Decimals should be 18");
-		expect(cap).to.eq(ethers.utils.parseEther("100"), "Cap should be 100 Tokens");
-	});
+    it("...should set the correct initial values", async () => {
+        const symbol = await tcapInstance.symbol();
+        const name = await tcapInstance.name();
+        const decimals = await tcapInstance.decimals();
+        const defaultOwner = await tcapInstance.owner();
+        const cap = await tcapInstance.cap();
+        expect(defaultOwner).to.eq(orchestratorInstance.address);
+        expect(symbol).to.eq("TCAP", "Symbol should equal TCAP");
+        expect(name).to.eq("Total Market Cap Token");
+        expect(decimals).to.eq(18, "Decimals should be 18");
+        expect(cap).to.eq(ethers.utils.parseEther("100"), "Cap should be 100 Tokens");
+    });
 
-	it("...should have the ERC20 standard functions", async () => {
-		const totalSupply = await tcapInstance.totalSupply();
-		expect(totalSupply).to.eq(0, "Total supply should be 0");
-		const balance = await tcapInstance.balanceOf(accounts[0]);
-		expect(balance).to.eq(0, "Balance should be 0");
-	});
+    it("...should have the ERC20 standard functions", async () => {
+        const totalSupply = await tcapInstance.totalSupply();
+        expect(totalSupply).to.eq(0, "Total supply should be 0");
+        const balance = await tcapInstance.balanceOf(accounts[0]);
+        expect(balance).to.eq(0, "Balance should be 0");
+    });
 
-	it("...should allow to approve tokens", async () => {
-		const amount = ethersProvider.utils.parseEther("100");
-		await tcapInstance.connect(owner).approve(accounts[1], amount);
-		const allowance = await tcapInstance.allowance(accounts[0], accounts[1]);
-		expect(allowance).to.eq(amount);
-	});
+    it("...should allow to approve tokens", async () => {
+        const amount = ethersProvider.utils.parseEther("100");
+        await tcapInstance.connect(owner).approve(accounts[1], amount);
+        const allowance = await tcapInstance.allowance(accounts[0], accounts[1]);
+        expect(allowance).to.eq(amount);
+    });
 
-	it("...shouldn't allow users to mint", async () => {
-		const amount = ethersProvider.utils.parseEther("1000000");
-		await expect(tcapInstance.mint(accounts[0], amount)).to.be.revertedWith(
-			"TCAP::onlyVault: caller is not a vault"
-		);
-	});
+    it("...shouldn't allow users to mint", async () => {
+        const amount = ethersProvider.utils.parseEther("1000000");
+        await expect(tcapInstance.mint(accounts[0], amount)).to.be.revertedWith(
+            "TCAP::onlyVault: caller is not a vault"
+        );
+    });
 
-	it("...shouldn't allow users to burn", async () => {
-		const amount = ethersProvider.utils.parseEther("1000000");
-		await expect(tcapInstance.burn(accounts[1], amount)).to.be.revertedWith(
-			"TCAP::onlyVault: caller is not a vault"
-		);
-	});
+    it("...shouldn't allow users to burn", async () => {
+        const amount = ethersProvider.utils.parseEther("1000000");
+        await expect(tcapInstance.burn(accounts[1], amount)).to.be.revertedWith(
+            "TCAP::onlyVault: caller is not a vault"
+        );
+    });
 });
--- test/governance/GovernorBeta.test.ts
+++ test/governance/GovernorBeta.test.ts
@@ -6,48 +6,48 @@ import { governanceFixture } from "./fixtures";
 import { DELAY } from "./utils";
 
 describe("GovernorBeta", () => {
-	// waffle;
-	// const provider = new MockProvider({
-	// 	ganacheOptions: {
-	// 		hardfork: "istanbul",
-	// 		mnemonic: "horn horn horn horn horn horn horn horn horn horn horn horn",
-	// 		gasLimit: 9999999,
-	// 	},
-	// });
-	const [wallet] = waffle.provider.getWallets();
-	const loadFixture = waffle.createFixtureLoader([wallet], waffle.provider);
+  // waffle;
+  // const provider = new MockProvider({
+  // 	ganacheOptions: {
+  // 		hardfork: "istanbul",
+  // 		mnemonic: "horn horn horn horn horn horn horn horn horn horn horn horn",
+  // 		gasLimit: 9999999,
+  // 	},
+  // });
+  const [wallet] = waffle.provider.getWallets();
+  const loadFixture = waffle.createFixtureLoader([wallet], waffle.provider);
 
-	let ctx: Contract;
-	let timelock: Contract;
-	let governorBeta: Contract;
-	beforeEach(async () => {
-		const fixture = await loadFixture(governanceFixture);
-		ctx = fixture.ctx;
-		timelock = fixture.timelock;
-		governorBeta = fixture.governorBeta;
-	});
+  let ctx: Contract;
+  let timelock: Contract;
+  let governorBeta: Contract;
+  beforeEach(async () => {
+    const fixture = await loadFixture(governanceFixture);
+    ctx = fixture.ctx;
+    timelock = fixture.timelock;
+    governorBeta = fixture.governorBeta;
+  });
 
-	it("...should test ctx", async () => {
-		const balance = await ctx.balanceOf(wallet.address);
-		const totalSupply = await ctx.totalSupply();
-		expect(balance).to.be.eq(totalSupply);
-	});
+  it("...should test ctx", async () => {
+    const balance = await ctx.balanceOf(wallet.address);
+    const totalSupply = await ctx.totalSupply();
+    expect(balance).to.be.eq(totalSupply);
+  });
 
-	it("...should set timelock", async () => {
-		const admin = await timelock.admin();
-		expect(admin).to.be.eq(governorBeta.address);
-		const pendingAdmin = await timelock.pendingAdmin();
-		expect(pendingAdmin).to.be.eq(constants.AddressZero);
-		const delay = await timelock.delay();
-		expect(delay).to.be.eq(DELAY);
-	});
+  it("...should set timelock", async () => {
+    const admin = await timelock.admin();
+    expect(admin).to.be.eq(governorBeta.address);
+    const pendingAdmin = await timelock.pendingAdmin();
+    expect(pendingAdmin).to.be.eq(constants.AddressZero);
+    const delay = await timelock.delay();
+    expect(delay).to.be.eq(DELAY);
+  });
 
-	it("...should set governor", async () => {
-		const votingPeriod = await governorBeta.votingPeriod();
-		expect(votingPeriod).to.be.eq(17280);
-		const timelockAddress = await governorBeta.timelock();
-		expect(timelockAddress).to.be.eq(timelock.address);
-		const ctxFromGovernor = await governorBeta.ctx();
-		expect(ctxFromGovernor).to.be.eq(ctx.address);
-	});
+  it("...should set governor", async () => {
+    const votingPeriod = await governorBeta.votingPeriod();
+    expect(votingPeriod).to.be.eq(17280);
+    const timelockAddress = await governorBeta.timelock();
+    expect(timelockAddress).to.be.eq(timelock.address);
+    const ctxFromGovernor = await governorBeta.ctx();
+    expect(ctxFromGovernor).to.be.eq(ctx.address);
+  });
 });
