4928c975b65c6eb091dec832e89ef8072a32279f
Crisgarner <@crisgarner>
2023-02-06 12:57:38 -0600

    updated scripts

--- deploy/arbitrum/008_initialize_jpgz.ts
+++ deploy/arbitrum/008_initialize_jpgz.ts
@@ -2,23 +2,35 @@ import { ethers as ethershardhat, hardhatArguments } from "hardhat";
 require("dotenv").config();
 
 module.exports = async ({ deployments }: any) => {
-    if (hardhatArguments.network === "arbitrum") {
-        let jpgzWETHVaultHandler = await deployments.get("jpgzWETHVaultHandler");
-        let jpgzDAIVaultHandler = await deployments.get("jpgzDAIVaultHandler");
-        let OrchestratorDeployment = await deployments.get("ArbitrumOrchestrator");
-        let l2MessageExecutorProxy = await deployments.get("L2MessageExecutorProxy");
-        let jpegz = await deployments.get("JPEGZ");
+  if (hardhatArguments.network === "arbitrum") {
+    let jpegzWETHVaultHandler = await deployments.get("jpegzWETHVaultHandler");
+    let jpegzDAIVaultHandler = await deployments.get("jpegzDAIVaultHandler");
+    let OrchestratorDeployment = await deployments.get("ArbitrumOrchestrator");
+    let l2MessageExecutorProxy = await deployments.get(
+      "L2MessageExecutorProxy"
+    );
+    let jpegz = await deployments.get("JPEGZ");
 
-        let orchestrator = await ethershardhat.getContractAt(
-            "ArbitrumOrchestrator",
-            OrchestratorDeployment.address
-        );
+    let orchestrator = await ethershardhat.getContractAt(
+      "ArbitrumOrchestrator",
+      OrchestratorDeployment.address
+    );
 
-        console.log("Adding jpegz vault Handlers");
-        await orchestrator.addTCAPVault(jpegz.address, jpgzWETHVaultHandler.address);
-        await orchestrator.addTCAPVault(jpegz.address, jpgzDAIVaultHandler.address);
-        await orchestrator.transferOwnership(l2MessageExecutorProxy.address);
-    }
+    console.log("Adding jpegz vault Handlers");
+    let tx = await orchestrator.addTCAPVault(
+      jpegz.address,
+      jpegzWETHVaultHandler.address
+    );
+    await tx.wait();
+    tx = await orchestrator.addTCAPVault(
+      jpegz.address,
+      jpegzDAIVaultHandler.address
+    );
+    await tx.wait();
+    tx = await orchestrator.transferOwnership(l2MessageExecutorProxy.address);
+    await tx.wait();
+    console.log("Transferred Ownership to DAO");
+  }
 };
 
 module.exports.tags = ["Initialize"];
