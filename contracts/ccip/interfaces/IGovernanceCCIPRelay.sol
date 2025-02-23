25b480c400cb1f27b998444c861841b3f7ee35a4
voith <voithjm1@gmail.com>
2025-02-24 02:44:12 +0530

    [cantina finding #9] added gasLimit to relayMessage

--- contracts/ccip/interfaces/IGovernanceCCIPRelay.sol
+++ contracts/ccip/interfaces/IGovernanceCCIPRelay.sol
@@ -26,6 +26,16 @@ interface IGovernanceCCIPRelay {
   /// @dev Error thrown when a provided address is the zero address.
   error AddressCannotBeZero();
 
+  /// @dev Thrown when the specified gas limit is too low to execute the transaction.
+  /// @param gasLimit The provided gas limit.
+  /// @param minGasLimit The minimum required gas limit.
+  error GasLimitTooLow(uint256 gasLimit, uint256 minGasLimit);
+
+  /// @dev Thrown when the specified gas limit exceeds the maximum allowed threshold.
+  /// @param gasLimit The provided gas limit.
+  /// @param maxGasLimit The maximum allowed gas limit.
+  error GasLimitTooHigh(uint256 gasLimit, uint256 maxGasLimit);
+
   /// @dev Error thrown when payload is empty.
   error PayloadCannotBeEmpty();
 
@@ -87,11 +97,13 @@ interface IGovernanceCCIPRelay {
 
   /// @notice Relays a governance message to the destination chain.
   /// @param destinationChainSelector The ccip chain selector of the destination chain for the message.
+  /// @param gasLimit the maximum amount of gas CCIP can consume to execute ccipReceive()
   /// @param target The target address to execute the message on.
   /// @param payload The calldata payload to execute.
   /// @return messageId The unique identifier of the sent message.
   function relayMessage(
     uint64 destinationChainSelector,
+    uint256 gasLimit,
     address target,
     bytes calldata payload
   ) external payable returns (bytes32 messageId);
